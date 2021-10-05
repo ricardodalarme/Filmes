ff9bf63ddc3851c37247f3e35b9c47dd39934d35
Ricardo Dalarme <ricardoweasley@hotmail.com>
2021-10-04 21:48:40 -0300

    Search box

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Container } from './styles';
+import { Container, SearchButton, SearchContainer, Input } from './styles';
 import Header from '../../components/header';
+import { Feather } from '@expo/vector-icons';
 
 function Home() {
   return (
     <Container>
-      <Header title="Home"/>
+      <Header title="Home" />
+      <SearchContainer>
+        <Input
+          placeholder="Procurar algo?"
+          placeholderTextColor="#ddd"
+        />
 
+        <SearchButton>
+          <Feather
+            name="search"
+            size={30}
+            color="#FFF"
+          />
+        </SearchButton>
+      </SearchContainer>
     </Container>
   );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -5,3 +5,29 @@ export const Container = styled.SafeAreaView`
     flex:1;
     padding: 4px 0;
 `;
+
+export const SearchContainer = styled.View`
+    flex-direction:row;
+    width: 100%;
+    height: 50;
+    align-items: center;
+    padding: 0 14px;
+    margin-bottom: 8px;
+`;
+
+export const SearchButton = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+    height: 50;
+    width: 15%;
+`;
+
+export const Input = styled.TextInput`
+    background-color: rgba(255,255,255,0.4);
+    width: 85%;
+    height: 50;
+    border-radius: 50px;
+    padding: 8px 15px;
+    font-size: 18px;
+    color: #FFF;
+`;
